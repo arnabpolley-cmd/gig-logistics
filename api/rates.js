9032790d004fa256ddc276f7785ca1fb6e813b4c
Arnab <arnab.polley@navsoft.in>
2026-03-24 19:06:16 +0530

    made changes on shipping price moree

--- api/rates.js
+++ api/rates.js
@@ -152,7 +152,7 @@ export default async function handler(req, res) {
           "Weight": (i.grams / 1000), // Converts grams to KG
           "IsVolumetric": false,
           "ShipmentType": 1,
-          "Value": Math.round((i.price * i.quantity) / 100), // Converts total value (kobo) to Naira
+          "Value": Math.round((i.price * i.quantity)),
           "Description": i.title || i.name
         }))
       })
