cb70a244f37047155836edc3d6e2106f2d907733
arnab <arnab.polley@navsoft.in>
2026-04-24 17:09:45 +0530

    added service name new onewe

--- api/rates.js
+++ api/rates.js
@@ -188,7 +188,7 @@ export default async function handler(req, res) {
         description: "Live calculated delivery rate"
       }]
     });
-
+ 
   } catch (error) {
     console.error("Critical Bridge Error:", error.message);
     return res.status(200).json({ rates: [] });
